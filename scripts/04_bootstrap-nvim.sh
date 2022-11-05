f6cd83a9fd57b62482b1384a4237cab4b2987abd
Pierre-Yves Aillet <pyaillet@gmail.com>
2022-11-05 16:15:00 +0000

    improve configuration

--- scripts/04_bootstrap-nvim.sh
+++ scripts/04_bootstrap-nvim.sh
@@ -2,5 +2,5 @@
 
 echo "🚀 Bootstrapping neovim"
 
-echo "\n\n" | nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+echo "\n\n" | nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 2>&1 >/dev/null
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 2>&1 >/dev/null
---
+++ scripts/compl/install_esp.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cargo install ldproxy
+cargo install espup
+
+espup -e 4.4

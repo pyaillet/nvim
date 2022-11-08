090a6a542be2f01702a2458f60519c662120ceaa
Pierre-Yves Aillet <pyaillet@gmail.com>
2022-11-08 21:27:12 +0000

    fix default shell to zsh and aliases

--- scripts/07_install-oh-my-zsh.sh
+++ scripts/07_install-oh-my-zsh.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+echo "exec zsh" >> ~/.bashrc
+
+echo "alias vi=nvim" >> ~/.zshrc
+echo "alias vim=nvim" >> ~/.zshrc
